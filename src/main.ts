5e4da23149c11fbdfab64d27043196ddd17ffd4d
lucky-rocky <1141881946@qq.com>
2021-09-06 22:25:06 +0800

    element国际化完成,准备迁移axios

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus'
 import 'element-plus/dist/index.css'
 import { i18n } from './i18n'
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(ElementPlus, { size: 'small', zIndex: 3000 })
+  .use(ElementPlus, { size: 'small', zIndex: 3000, locale: zhCn })
   .use(i18n)
   .mount('#app')
